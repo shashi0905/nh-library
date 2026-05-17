f2647005ba0a83849620d1c8bcd18543d1ba2f77
shashi <0905shashi@gmail.com>
2026-05-18 03:06:37 +0530

    fixing formatting issues causing pipeline failure

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -58,4 +58,3 @@ async def client() -> AsyncGenerator[AsyncClient, None]:
     transport = ASGITransport(app=app)
     async with AsyncClient(transport=transport, base_url="http://test") as async_client:
         yield async_client
-
