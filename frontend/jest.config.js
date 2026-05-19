394a6f5c1a9603f7c374b55dec2a58a529d684a4
shashi <0905shashi@gmail.com>
2026-05-20 01:30:22 +0530

    adding jest config file

---
+++ frontend/jest.config.js
@@ -0,0 +1,12 @@
+const nextJest = require("next/jest.js");
+
+const createJestConfig = nextJest({ dir: "./" });
+
+const config = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: { "^@/(.*)$": "<rootDir>/src/$1" },
+  coverageThreshold: { global: { lines: 70 } },
+};
+
+module.exports = createJestConfig(config);
